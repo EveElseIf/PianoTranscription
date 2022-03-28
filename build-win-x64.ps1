29bfb517d50d2891b91d8f5688e25db6b5b0863d
EveElseIf <EveElseIf@outlook.com>
2022-03-29 00:47:27 +0800

    update build script

---
+++ build-win-x64.ps1
@@ -0,0 +1,15 @@
+$onnx_exists=([System.IO.File]::Exists("./PianoTranscription.Core/transcription.onnx"))
+Write-Output "onnx exists="$onnx_exists
+Get-ChildItem
+if (-not $onnx_exists) {
+    Invoke-WebRequest -Uri "https://github.com/EveElseIf/PianoTranscription/releases/download/ONNX/transcription.onnx" -OutFile "./PianoTranscription.Core"
+}
+# dotnet publish PianoTranscription -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+# $publish_path = "PianoTranscription/bin/Release/net6.0/win-x64/publish/"
+# Remove-Item $publish_path"Melanchall_DryWetMidi_Native32.dll"
+# Remove-Item $publish_path"Melanchall_DryWetMidi_Native64.dll"
+# Remove-Item $publish_path"Melanchall_DryWetMidi_Native64.dylib"
+# Remove-Item $publish_path"onnxruntime_providers_shared.lib"
+# Remove-Item $publish_path"onnxruntime.lib"
+# Remove-Item $publish_path"PianoTranscription.Core.pdb"
+# Remove-Item $publish_path"PianoTranscription.pdb"
