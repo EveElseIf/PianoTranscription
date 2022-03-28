e5cb1c18ad66dcef4bb1355619aace96fcdb371e
EveElseIf <EveElseIf@outlook.com>
2022-03-29 01:28:50 +0800

    update build script

---
+++ build-linux-x64.sh
@@ -0,0 +1,11 @@
+if [ ! -f "./PianoTranscription.Core/transcription.onnx" ]
+    then
+        wget -O "./PianoTranscription.Core/transcription.onnx" "https://github.com/EveElseIf/PianoTranscription/releases/download/ONNX/transcription.onnx"
+fi
+dotnet publish PianoTranscription -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+publish_path="PianoTranscription/bin/Release/net6.0/linux-x64/publish/"
+rm $publish_path"Melanchall_DryWetMidi_Native32.dll"
+rm $publish_path"Melanchall_DryWetMidi_Native64.dll"
+rm $publish_path"Melanchall_DryWetMidi_Native64.dylib"
+rm $publish_path"PianoTranscription.Core.pdb"
+rm $publish_path"PianoTranscription.pdb"
