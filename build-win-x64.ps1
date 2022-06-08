efda02bd89cf55f53287d64f1aed876dc895b470
Eve ElseIf <EveElseIf@outlook.com>
2022-06-08 22:34:34 +0800

    add vorbis to ffmpeg, update build script

--- build-win-x64.ps1
+++ build-win-x64.ps1
@@ -8,6 +8,7 @@ Copy-Item "PianoTranscription.Core/ffmpeg-win-x64/ffmpeg.exe" "PianoTranscriptio
 Copy-Item "PianoTranscription.Core/ffmpeg-win-x64/ffmpeg.exe" "PianoTranscription.App/bin/Debug/net6.0"
 if ($args[0] -eq "dist") {
     Write-Output "Start build dist"
+
     dotnet publish PianoTranscription -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
     $publish_path = "PianoTranscription/bin/Release/net6.0/win-x64/publish/"
     Copy-Item "PianoTranscription.Core/ffmpeg-win-x64/ffmpeg.exe" $publish_path
@@ -26,5 +27,25 @@ if ($args[0] -eq "dist") {
     New-Item -ItemType Directory $dist_path
     Copy-Item -Path "$publish_path*" $dist_path
     Compress-Archive $dist_path "$out_dir_name.zip" -Force
+
+    dotnet publish PianoTranscription.App -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+    $publish_path = "PianoTranscription.App/bin/Release/net6.0/win-x64/publish/"
+    Copy-Item "PianoTranscription.Core/ffmpeg-win-x64/ffmpeg.exe" $publish_path
+    Remove-Item $publish_path"Melanchall_DryWetMidi_Native32.dll"
+    Remove-Item $publish_path"Melanchall_DryWetMidi_Native64.dll"
+    Remove-Item $publish_path"Melanchall_DryWetMidi_Native64.dylib"
+    Remove-Item $publish_path"onnxruntime_providers_shared.lib"
+    Remove-Item $publish_path"onnxruntime.lib"
+    Remove-Item $publish_path"PianoTranscription.Core.pdb"
+    Remove-Item $publish_path"PianoTranscription.App.pdb"
+    $out_dir_name = "pianotranscription-win-x64-gui"
+    $dist_path = "build/$out_dir_name"
+    if ([System.IO.Directory]::Exists($dist_path)) {
+        Remove-Item $dist_path -Force -Recurse
+    }
+    New-Item -ItemType Directory $dist_path
+    Copy-Item -Path "$publish_path*" $dist_path
+    Compress-Archive $dist_path "$out_dir_name.zip" -Force
+
     Write-Output "Build dist Finished"
 }
